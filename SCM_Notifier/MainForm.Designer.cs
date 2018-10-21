a09a28524eb94e0baa6df683de78e6ef3e9b3fc0
ZAM <ZAM@1CLICK.LV>
2018-10-21 09:45:33 +0300

    SortList functionality added

--- SCM_Notifier/MainForm.Designer.cs
+++ SCM_Notifier/MainForm.Designer.cs
@@ -77,7 +77,9 @@ namespace pocorall.SCM_Notifier
 			this.fetchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem ();
 			this.toolStripMenuItem9 = new System.Windows.Forms.ToolStripSeparator ();
 			this.propertiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem ();
-			this.statusStrip = new System.Windows.Forms.StatusStrip ();
+      this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+      this.sortListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.statusStrip = new System.Windows.Forms.StatusStrip();
 			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel ();
 			this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel ();
 			this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel ();
@@ -341,7 +343,9 @@ namespace pocorall.SCM_Notifier
             this.logToolStripMenuItem,
             this.fetchToolStripMenuItem,
             this.toolStripMenuItem9,
-            this.propertiesToolStripMenuItem});
+            this.propertiesToolStripMenuItem,
+            this.toolStripSeparator3, 
+            this.sortListToolStripMenuItem});
 			this.contextMenuStrip.Name = "contextMenuStrip";
 			this.contextMenuStrip.Size = new System.Drawing.Size (155, 182);
 			this.contextMenuStrip.Opening += new System.ComponentModel.CancelEventHandler (this.contextMenuStrip_Opening);
@@ -421,7 +425,14 @@ namespace pocorall.SCM_Notifier
 			this.propertiesToolStripMenuItem.Size = new System.Drawing.Size (154, 22);
 			this.propertiesToolStripMenuItem.Text = "Properties";
 			this.propertiesToolStripMenuItem.Click += new System.EventHandler (this.propertiesToolStripMenuItem_Click);
-			//
+      // 
+      // sortListToolStripMenuItem
+      // 
+      this.sortListToolStripMenuItem.Name = "sortListToolStripMenuItem";
+      this.sortListToolStripMenuItem.Size = new System.Drawing.Size(209, 22);
+      this.sortListToolStripMenuItem.Text = "Sort list (Asc, Desc, None)";
+      this.sortListToolStripMenuItem.Click += new System.EventHandler(this.sortListToolStripMenuItem_Click);
+      //
 			// statusStrip
 			//
 			this.statusStrip.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
